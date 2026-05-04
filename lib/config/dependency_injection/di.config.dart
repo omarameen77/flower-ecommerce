ddeb30144027f8e71f3e7dd74c73174f1237b5c9
Omar Ameen <omarxwork10@gmail.com>
2026-05-05 00:02:08 +0300

    refactor: enhance BaseState class with copyWith method and Equatable implementation; add UserEntity model; update AuthApi for dependency injection

--- lib/config/dependency_injection/di.config.dart
+++ lib/config/dependency_injection/di.config.dart
@@ -9,11 +9,10 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flower/config/dependency_injection/injectable_test.dart'
-    as _i782;
-import 'package:flower/core/network/safe_api_caller.dart' as _i781;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+
+import '../../core/network/safe_api_caller.dart' as _i563;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -22,8 +21,7 @@ extension GetItInjectableX on _i174.GetIt {
     _i526.EnvironmentFilter? environmentFilter,
   }) {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
-    gh.factory<_i782.TestService>(() => _i782.TestService());
-    gh.factory<_i781.SafeApiCaller>(() => _i781.SafeApiCaller());
+    gh.factory<_i563.SafeApiCaller>(() => _i563.SafeApiCaller());
     return this;
   }
 }
