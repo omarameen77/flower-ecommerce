922c794ceb00494608ee6d79b634800e9f9634f8
Omar Ameen <omarxwork10@gmail.com>
2026-05-09 16:13:54 +0300

    feat(auth): implement login functionality with API integration

--- lib/features/auth/domain/repositories/auth_repo.dart
+++ lib/features/auth/domain/repositories/auth_repo.dart
@@ -3,9 +3,12 @@ import 'package:flower/core/network/model/user_entity.dart';
 import 'package:flower/features/auth/domain/entities/forget_password_entity.dart';
 import 'package:flower/features/auth/domain/entities/reset_password_entity.dart';
 import 'package:flower/features/auth/domain/entities/verify_reset_code_entity.dart';
+import 'package:flower/features/auth/domain/use_cases/login_params.dart';
 import 'package:flower/features/auth/domain/use_cases/register_params.dart';
 
 abstract class AuthRepo {
+  Future<BaseResponse<UserEntity>> login(LoginParams params);
+
   Future<BaseResponse<UserEntity>> register(RegisterParams params);
 
   Future<BaseResponse<ForgetPasswordEntity>> forgetPassword({
