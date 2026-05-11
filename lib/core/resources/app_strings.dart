a3f8b4a21ec035872cad7f25217ec976df8a9503
Ziad Sleem <ziad.sleem.111@gmail.com>
2026-05-11 21:01:06 +0300

    feat(core): add app strings constants
    Introduce a new AppStrings class in core/resources to centralize string constants for limit, page, add to cart, and no name labels.

---
+++ lib/core/resources/app_strings.dart
@@ -0,0 +1,6 @@
+abstract class AppStrings {
+  static const String limit = 'limit';
+  static const String noNmae = 'No name';
+  static const String page = 'page';
+  static const String addToCart = 'Add to cart';
+}
