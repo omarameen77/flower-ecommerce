3d637cd8e7cf0dc98427643295166cf7011e05d8
Omar Ameen <omarxwork10@gmail.com>
2026-05-31 10:15:53 +0300

    feat: add Firebase integration and notification permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flower/app.dart';
 import 'package:flower/config/dependency_injection/di.dart';
 import 'package:flower/core/network/dio_helper.dart';
+import 'package:flower/firebase_options.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await EasyLocalization.ensureInitialized();
   DioHelper.init();
   configureDependencies();
+  // // استدعاء وتشغيل الـ Notification Service
+  // final notificationService = NotificationService();
+  // await notificationService.requestPermissions();
+  // await notificationService.initLocalNotifications();
+  // notificationService.listenToNotifications();
+  // await notificationService
+  //     .getDeviceToken(); // عشان يطبعلك الـ Token في الـ Console
 
   runApp(
     EasyLocalization(
