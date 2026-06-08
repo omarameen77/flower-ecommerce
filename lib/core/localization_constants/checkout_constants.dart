bb8ad2df0887d1f6c8a7ec245c5b0b4328b832b4
Ziad Sleem <ziad.sleem.111@gmail.com>
2026-06-08 14:41:19 +0300

    feat: implement checkout and order management features with associated data layers and UI components

---
+++ lib/core/localization_constants/checkout_constants.dart
@@ -0,0 +1,28 @@
+import 'package:easy_localization/easy_localization.dart';
+
+abstract class CheckoutConstants {
+  CheckoutConstants._();
+
+  static String get title => 'checkout.title'.tr();
+  static String get shippingAddress => 'checkout.shipping_address'.tr();
+  static String get addNewAddress => 'checkout.add_new_address'.tr();
+  static String get paymentMethod => 'checkout.payment_method'.tr();
+  static String get cashOnDelivery => 'checkout.cash_on_delivery'.tr();
+  static String get creditCard => 'checkout.credit_card'.tr();
+  static String get sendAsGift => 'checkout.send_as_gift'.tr();
+  static String get receiverName => 'checkout.receiver_name'.tr();
+  static String get receiverPhone => 'checkout.receiver_phone'.tr();
+  static String get receiverAddress => 'checkout.receiver_address'.tr();
+  static String get placeOrder => 'checkout.place_order'.tr();
+  static String get orderPlaced => 'checkout.order_placed'.tr();
+  static String get thankYou => 'checkout.thank_you'.tr();
+  static String get thankYouMessage => 'checkout.thank_you_message'.tr();
+  static String get continueShopping => 'checkout.continue_shopping'.tr();
+  static String get subtotal => 'checkout.subtotal'.tr();
+  static String get delivery => 'checkout.delivery'.tr();
+  static String get total => 'checkout.total'.tr();
+  static String get egp => '\$ ';
+  static String get deliveryTime => 'checkout.delivery_time'.tr();
+  static String get instant => 'checkout.instant'.tr();
+  static String get paymentCompleted => 'checkout.payment_completed'.tr();
+}
