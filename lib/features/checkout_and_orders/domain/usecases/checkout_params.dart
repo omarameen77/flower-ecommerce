6a6ae84ffd7b34078c94e0c15b0957f2a0d32bbb
Ziad Sleem <ziad.sleem.111@gmail.com>
2026-06-11 14:21:45 +0300

    feat: implement checkout and order management features including domain models, use cases, cubits, and UI components.

---
+++ lib/features/checkout_and_orders/domain/usecases/checkout_params.dart
@@ -0,0 +1,15 @@
+class CheckoutParams {
+  final String street;
+  final String phone;
+  final String city;
+  final String lat;
+  final String long;
+
+  CheckoutParams({
+    required this.street,
+    required this.phone,
+    required this.city,
+    required this.lat,
+    required this.long,
+  });
+}
