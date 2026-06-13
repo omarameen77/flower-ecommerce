2ada0dd1f6e76af9e0711263d4615c648da535b4
Ziad Sleem <ziad.sleem.111@gmail.com>
2026-06-13 16:54:35 +0300

    refactor: remove unused configuration file and associated logic

--- lib/features/address/domain/repositories/current_location_repo.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flower/features/address/domain/entities/current_location.dart';
-
-abstract interface class CurrentLocationRepo {
-  Future<CurrentLocation> getCurrent();
-}
--- lib/features/address/domain/repositories/egypt_location_repo.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flower/features/address/domain/entities/location_item.dart';
-
-abstract interface class EgyptLocationRepo {
-  Future<List<CityItem>> getCities();
-  Future<List<AreaItem>> getAreas();
-}
